d7b74010500769b1a3d1f32eb2171c89bb294a0d
Daniel Carrillo Herrera <danielcarrillohcse@gmail.com>
2018-01-25 23:36:38 -0500

    Hasta el video 8. Arreglar el problema del registro del nuevo usuario.

---
+++ app/src/main/java/uber/uber/example/com/uber2/Classes/Users.java
@@ -0,0 +1,16 @@
+package uber.uber.example.com.uber2.Classes;
+
+/**
+ * Created by daniel on 25/01/18.
+ */
+
+public class Users {
+
+    String name, email, password;
+
+    public Users(String name, String email, String password){
+            this.name = name;
+            this.email = email;
+            this.password = password;
+    }
+}
--- app/src/main/java/uber/uber/example/com/uber2/SignUpActivity.java
+++ app/src/main/java/uber/uber/example/com/uber2/SignUpActivity.java
@@ -13,12 +13,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import uber.uber.example.com.uber2.Classes.Users;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText e5_name, e6_email, e7_password;
     FirebaseAuth auth;
     ProgressDialog dialog;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +62,27 @@ public class SignUpActivity extends AppCompatActivity {
 
                                 dialog.hide();
                                 Toast.makeText(getApplicationContext(), "User registered succesfull", Toast.LENGTH_SHORT).show();
+
+                                databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+                                Users  user_object = new Users(e5_name.getText().toString(),
+                                                               e6_email.getText().toString(),
+                                                               e7_password.getText().toString());
+
+                                FirebaseUser firebaseUser = auth.getCurrentUser();
+                                databaseReference.child(firebaseUser.getUid()).setValue(user_object)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                    if(task.isSuccessful()){
+
+                                                        Toast.makeText(getApplicationContext(), "User data saved", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                    else
+                                                    {
+                                                        Toast.makeText(getApplicationContext(), "User data could not be saved", Toast.LENGTH_SHORT).show();
+                                                    }
+                                            }
+                                        });
 
                             }
                             else{
